f2d955498422518fda75fdf745be4c3d872a08a0
Patrick <panpog@gmail.com>
2016-07-20 21:35:07 -0700

    Clean up

--- src/Main.cpp
+++ src/Main.cpp
@@ -50,26 +50,18 @@ void react(std::vector<std::unique_ptr<Potion>>&v) {
 
 int main() {
 	std::vector<std::unique_ptr<Potion>> v;
-	if (true) {
-		if (R(new Base("foo")) == E(new Base("foo")))
-			std::cout << "fail 1";
-		if (Base("foo") == E(new Base("foo")))
-			std::cout << "fail 2";
-		v.push_back(make(new Base("foo")));
-		v.push_back(make(new Base("bar")));
-		v.push_back(make(new Base("A thing")));
-		v.push_back(make(new Base("Another thing")));
-		v.push_back(make(new R(new Base("foo"))));
-		v.push_back(make(new E(new Base("foo"))));
-		//v.push_back(make(new E(new Base("bar"))));
-		//std::cout <<static_cast<std::string>(*make(new E(new Base("bar"))));
-		react(v);
-		print(v);
-	} else {
-		v.push_back(make(new R(new Base("foo"))));
-		v.push_back(make(new E(new Base("foo"))));
-		react(v);
-		print(v);
-	}
+	if (R(new Base("foo")) == E(new Base("foo")))
+		std::cout << "fail 1";
+	if (Base("foo") == E(new Base("foo")))
+		std::cout << "fail 2";
+	v.push_back(make(new Base("foo")));
+	v.push_back(make(new Base("bar")));
+	v.push_back(make(new Base("A thing")));
+	v.push_back(make(new Base("Another thing")));
+	v.push_back(make(new R(new Base("foo"))));
+	v.push_back(make(new E(new Base("foo"))));
+	v.push_back(make(new E(new Base("bar"))));
+	react(v);
+	print(v);
 	return 0;
 }
